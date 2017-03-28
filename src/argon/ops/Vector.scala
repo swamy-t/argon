de38e5cc9549d17d67b4bc1225844e6431aa518c
Ruben Fiszel <ruben.fiszel@epfl.ch>
2017-03-28 22:06:52 +0200

    Vector macro v1 - all the heavy work done

---
+++ src/argon/ops/Vector.scala
@@ -0,0 +1,20 @@
+package argon.ops
+
+import macros.{vectorApiX, vectorCompX, vectorX}
+import argon.core.Staging
+import argon.typeclasses._
+
+@vectorApiX
+trait VectorApi {
+  20
+}
+
+@vectorX
+trait VectorExp {
+  20
+}
+
+@vectorCompX
+object Vector {
+  20
+}
